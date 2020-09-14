c4e3da743ae44f0317c697c1469175fc2d717322
Jah <moskovienkor@gmail.com>
2020-09-14 17:11:27 +0300

    hw27.1: - redundant files deleted from repository;

--- src/main/java/internet/shop/controller/user/DeleteUsersController.java
+++ src/main/java/internet/shop/controller/user/DeleteUsersController.java
@@ -23,7 +23,7 @@ public class DeleteUsersController extends HttpServlet {
         String id = req.getParameter("id");
         Long longId = Long.valueOf(id);
         userService.delete(USER_ID);
-        shoppingCartService.delete(longId);
+        shoppingCartService.delete(USER_ID);
         resp.sendRedirect(req.getContextPath() + "/users/all");
     }
 }
